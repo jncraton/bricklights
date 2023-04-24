ddbaeff6466331bb5b59aa5c2ef3b78300ed93dc
Jon Craton <jncraton@gmail.com>
2023-04-24 11:40:21 -0400

    Add parent class

--- bricklights.py
+++ bricklights.py
@@ -4,26 +4,21 @@ from pybricks.tools import wait
 
 from urandom import randint
 
-class Flame:
-    def __init__(self, port, delay=8, intensity=30):
-        self.light = Light(port)
-        self.delay = delay
-        self.intensity = intensity
-        self.time = 0
-
-    def update(self):
-        self.time += 1
-
-        if self.time % self.delay == 0:
-            self.light.on(randint(self.intensity-25,self.intensity))
-
-class Fader:
-    def __init__(self, port, speed=1, intensity=100):
+class ManagedLight:
+    def __init__(self, port, speed=1.0, intensity=1.0):
         self.light = Light(port)
         self.speed = speed
         self.intensity = intensity
         self.time = 0
 
+class Flame(ManagedLight):
+    def update(self):
+        self.time += 1
+
+        if self.time % 8*self.speed == 0:
+            self.light.on(randint(int(self.intensity*100-25),int(self.intensity*100)))
+
+class Fader(ManagedLight):
     def update(self):
         self.time += 1
 
@@ -31,7 +26,7 @@ class Fader:
 
         brightness = step if step < 100 else 200 - step
 
-        self.light.on(brightness*self.intensity/100)
+        self.light.on(brightness*self.intensity)
 
 light1 = Flame(Port.A)
 light2 = Flame(Port.C)
