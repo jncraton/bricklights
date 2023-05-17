04a15bc78269f9971b719aa658cbeba7d986eada
Jon Craton <jncraton@gmail.com>
2023-05-17 11:46:47 -0400

    Create cityflames.py

---
+++ cityflames.py
@@ -0,0 +1,16 @@
+from pybricks.parameters import Port, Color
+from pybricks.hubs import CityHub
+from pybricks.tools import wait
+
+from bricklights import Flame, Crossfader, RGBFlame
+
+lights = [
+    Flame(Port.A),
+    Flame(Port.B),
+    RGBFlame(CityHub().light, Color(30,100,100))
+]
+
+while 1:
+    wait(10)
+    for light in lights:
+        light.update()
