d2d0300e5d87d339e2bafbbec16f5739c2ff1a13
Jon Craton <jncraton@gmail.com>
2023-05-18 15:31:59 -0400

    Clean up example code

--- cityflames.py
+++ cityflames.py
@@ -4,9 +4,13 @@ from pybricks.tools import wait
 
 from bricklights import Flame, RGBFlame
 
-lights = [Flame(Port.A), Flame(Port.B), RGBFlame(CityHub().light, Color(30, 100, 100))]
+lights = [
+    Flame(Port.A),
+    Flame(Port.B),
+    RGBFlame(CityHub().light, Color(30, 100, 100)),
+]
 
-while 1:
+while True:
     wait(10)
     for light in lights:
         light.update()
